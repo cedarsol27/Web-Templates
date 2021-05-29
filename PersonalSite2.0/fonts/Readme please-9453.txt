c4e2cae95770f92ebb0c1da721f11ade907ba988
James Pesta <cedarsol27@users.noreply.github.com>
2021-05-29 12:53:56 -0700

    Some templates for web design

---
+++ PersonalSite2.0/fonts/Readme please-9453.txt
@@ -0,0 +1,37 @@
+This font is free for all use. 
+
+All the vectors were drawn by me and I release the font for free for any use.
+
+Help my work, all donations are greatly appreciated.
+
+Even with a like on my facebook page can help me.
+
+https://www.facebook.com/southype <- follow me ;)
+
+Paypal: Southype@gmail.com
+
+A Moon
+B Sun
+C Venus
+D Mercury
+E Mars
+F Jupiter
+G Saturn
+H Uranus
+I Uranus
+J Neptune
+K Ceres
+L Earth
+M Earth
+N Palas
+O Juno
+P 5 Astrea
+Q Vesta
+R Palas
+T Pluto
+U Sedna
+V Eris
+W Makemake
+X Orcus
+Y Ixion
+Z Moon
---
+++ ZoomNav/script.js
@@ -0,0 +1,34 @@
+onload = () =>{ // Called arrow function expression. Similar to function, has some limitations
+
+
+    const btnNav = document.querySelector('.btn-nav'); // Selects the first element with specified name
+    const nav = document.querySelector('.nav');
+    const navItems = document.querySelectorAll('.nav-items'); // Selects all elements with specified name
+
+    let navClose = true;
+
+    const handlerNavigation = () =>{
+
+        let scale = 1, translateY = 0;
+
+        btnNav.classList.toggle('ri-close-line'); // Pulls a class (classList), assigns it to another, toggle on or off (.toggle)
+        btnNav.classList.toggle('ri-menu-line'); 
+
+        nav.classList.toggle('nav-open');
+        nav.classList.toggle('nav-close');
+
+        if(!navClose){
+            scale = 4;
+            translateY = -100;
+        }
+        navItems.forEach( (item,index)=>{
+            item.setAttribute('style',`transition-delay:.${ index + 1 }s; transform: translateY(${translateY}vh) scale(${scale})`); // I think this is saying for each link in nav, apply a transition delay of adding one second additional from the previous nav, and apply a css style
+        });
+
+        navClose = !navClose; // switches true to false
+    }
+
+    // Event added
+
+    btnNav.addEventListener('click',handlerNavigation); // When the button is clicked, run through the function of handlerNavigation. EventListener is this action
+}
